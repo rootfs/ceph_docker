f7a6b5dc3cb5996702d8fe0cd28fa8eb2c34184a
root <root@gprfc027.sbu.lab.eng.bos.redhat.com>
2015-04-13 15:00:50 -0400

    fix and add
    Signed-off-by: root <root@gprfc027.sbu.lab.eng.bos.redhat.com>

--- init.sh
+++ init.sh
@@ -2,23 +2,9 @@
 #set -e
 set -x
 
-rm -f /etc/ceph/ceph*
+rm -f /etc/ceph/*
 
-#install_opt="--dev giant"
-NODES=`hostname`
-#on admin node, create ssh key and deploy on all NODES  
-CLUSTER=""
-for i in ${NODES[@]}
-do
-    echo $i
-    CLUSTER=`echo -n $i $CLUSTER`
-done
-
-# choose the last node as master
-MASTER=$i
-
-
-#ceph-deploy install ${install_opt} ${CLUSTER}
+MASTER=`hostname -s`
 
 #create ceph cluster
 ceph-deploy --overwrite-conf new ${MASTER}  
@@ -26,7 +12,7 @@ ceph-deploy --overwrite-conf mon create-initial ${MASTER}
 ceph-deploy --overwrite-conf mon create ${MASTER}
 ceph-deploy  gatherkeys ${MASTER}  
 
-for i in 0 1
+for i in 0 1 2
 do
 	ceph osd create
 	ceph-osd -i ${i} --mkfs --mkkey
@@ -35,40 +21,24 @@ do
 	ceph-osd -i ${i} -k /var/lib/ceph/osd/ceph-${i}/keyring
 done
 
-set -e 
-#ceph-deploy --overwrite-conf admin ${CLUSTER}
 
-#start ceph on all NODES  
-service ceph restart
+#start ceph 
+#service ceph restart
 
 ps -ef |grep ceph
 
 #see if we are ready to go  
-#osd tree should show all osd are up  
 ceph osd tree  
 #ceph health should be clean+ok  
 ceph health  
-# create a pool
-#ceph osd pool create kube 16
-#rados -p kube ls  
 
 # create ceph fs
-ceph-deploy mds create ${MASTER}
 ceph osd pool create cephfs_data 4
 ceph osd pool create cephfs_metadata 4
 ceph fs new cephfs cephfs_metadata cephfs_data
-
-service ceph restart
-
-ceph osd pool create kube 4
+ceph-deploy mds create ${MASTER}
 
 ps -ef |grep ceph
 
-#mount ceph fs  
-#yum install -y ceph-fuse  
-#mkdir -p /mnt/ceph 
-#timeout 60s ceph-fuse -m ${MASTER}:6789 /mnt/ceph
-#echo "mount status " $?
-#should see cephfs mounted
 
 ceph -w
--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ mkdir -p /home/etc
 mkdir -p /home/etc/ceph
 mkdir -p /home/disks
 
-for i in 0 1
+for i in 0 1 2
 do
   umount /tmp/ceph_disk${i}
   dd if=/dev/zero of=/home/disks/d${i} bs=1024M count=6 conv=notrunc
@@ -12,6 +12,6 @@ do
   mount -t xfs -o loop  /home/disks/d${i} /tmp/ceph_disk${i}
 done
 
-docker run --privileged --net=host -i -t  -v /tmp/ceph_disk0:/var/lib/ceph/osd/ceph-0 -v /tmp/ceph_disk1:/var/lib/ceph/osd/ceph-1  -v /etc/ceph:/etc/ceph  -t centos_ceph_pkg /bin/bash /init.sh
+docker run --privileged --net=host -i -t  -v /tmp/ceph_disk0:/var/lib/ceph/osd/ceph-0 -v /tmp/ceph_disk1:/var/lib/ceph/osd/ceph-1 -v /tmp/ceph_disk2:/var/lib/ceph/osd/ceph-2  -v /etc/ceph:/etc/ceph  -t centos_ceph_pkg /bin/bash /init.sh
 
 
